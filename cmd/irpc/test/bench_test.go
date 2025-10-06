17619395a69a0f8d75b4195f6f32722349138ce2
Martin Benes <martinbenesh@gmail.com>
2025-10-06 12:30:38 +0200

    more runs on int64 benchmark. like on the binmarshal benchmark

--- cmd/irpc/test/bench_test.go
+++ cmd/irpc/test/bench_test.go
@@ -72,6 +72,8 @@ func BenchmarkAddInt64(b *testing.B) {
 	service := newBasicAPIIRpcService(basicApiImpl{skew: skew})
 	serviceEp.RegisterService(service)
 
+	callsN := []int{1, 10, 100}
+
 	c, err := newBasicAPIIRpcClient(clientEp)
 	if err != nil {
 		b.Fatalf("failed to create client: %v", err)
@@ -79,23 +81,24 @@ func BenchmarkAddInt64(b *testing.B) {
 
 	b.ResetTimer()
 	crw.Reset()
-	for range b.N {
-		var x, y int64 = 1, 10
-		if res := c.addInt64(x, y); res != x+y+int64(skew) {
-			b.Fatalf("%d + %d != %d", x, y, res)
-		}
+	for _, cn := range callsN {
+		b.Logf("making %d calls", cn)
+		b.Run(fmt.Sprintf("runs-%d", cn), func(b *testing.B) {
+			crw.Reset()
+			for range b.N {
+				for range cn {
+					var x, y int64 = 1, 10
+					if res := c.addInt64(x, y); res != x+y+int64(skew) {
+						b.Fatalf("%d + %d != %d", x, y, res)
+					}
+				}
+			}
+			b.ReportMetric(float64(crw.RBytes())/float64(b.N), "rBytes/rpc")
+			b.ReportMetric(float64(crw.WBytes())/float64(b.N), "wBytes/rpc")
+		})
 	}
-	// b.Logf("N: %d, rBytes: %d, wBytes: %d", b.N, crw.RBytes(), crw.WBytes())
-	b.ReportMetric(float64(crw.RBytes())/float64(b.N), "rBytes/rpc")
-	b.ReportMetric(float64(crw.WBytes())/float64(b.N), "wBytes/rpc")
 
 	if err := clientEp.Close(); err != nil {
 		b.Fatalf("clientEp.Close(): %v", err)
 	}
-	// if err := serviceEp.Close(); err != nil {
-	// 	b.Fatalf("serviceEp.Close(): %+v", err)
-	// }
-	// if err := clientEp.Close(); err != nil {
-	// 	b.Fatalf("clientEp.Close(): %+v", err)
-	// }
 }
